7696fa520f212954ed9cf206970f60a83b11e433
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-10-19 21:59:18 +0600

    Create Script.js

---
+++ diagonalDifference/Script.js
@@ -0,0 +1,17 @@
+function diagonalDifference(arr) {
+var n = arr.length;
+var diag1 = 0;
+var diag2 = 0; 
+for(var i=0; i<n; i++){ 
+     for(var j=0; j<n; j++){
+         if(i === j) { 
+            diag1 += arr[i][j];
+            } 
+           if(j=(arr.length)-1-i){
+              diag2 += arr[i][j];
+         } 
+        } 
+    } 
+    return Math.abs(diag1 - diag2);
+
+}
