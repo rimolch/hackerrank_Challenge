f6437d728c14fa0b508fcb740fb2defca303c457
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-10-20 07:29:05 +0600

    Update Script.js

--- diagonalDifference/Script.js
+++ diagonalDifference/Script.js
@@ -4,14 +4,14 @@ var diag1 = 0;
 var diag2 = 0; 
 for(var i=0; i<n; i++){ 
      for(var j=0; j<n; j++){
-         if(i === j) { 
+         if(i === j) { // for primary diagonal
             diag1 += arr[i][j];
             } 
-           if(j=(arr.length)-1-i){
+           if(j=(arr.length)-1-i){ // for secondary diagonal
               diag2 += arr[i][j];
          } 
         } 
     } 
-    return Math.abs(diag1 - diag2);
+    return Math.abs(diag1 - diag2);// this will show tha absolute value of two difference  diagonal
 
 }
