6d52593937ee14ff1318a42cb1ed5e8899119fc9
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-11-01 11:22:02 +0600

    Update Script.js

--- birthdaycakeCandles/Script.js
+++ birthdaycakeCandles/Script.js
@@ -1,1 +1,11 @@
 
+function birthdayCakeCandles(candles) {
+var height = Math.max(...candles);
+var blowOut = 0;
+ for(let i = 0;i < candles.length;i++){
+        if(candles[i] == height){
+            blowOut = 2;
+        }
+    }
+    return blowOut;
+}
